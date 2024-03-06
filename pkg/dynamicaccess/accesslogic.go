382beb9b17a811ed28c9c6102eb121f73463d27f
Peter Ott <ottpeter@tuta.io>
2024-03-06 16:57:25 +0100

    Adds pseudo code for Access Logic

--- pkg/dynamicaccess/accesslogic.go
+++ pkg/dynamicaccess/accesslogic.go
@@ -18,8 +18,26 @@ type DefaultAccessLogic struct {
 }
 
 // Will give back Swarm reference with symmertic encryption key (128 byte)
-func (al *DefaultAccessLogic) Get(encryped_ref string, publisher string, tag string) (string, error) {
-	return "", nil
+// @publisher: public key
+func (al *DefaultAccessLogic) Get(act_root_hash string, encryped_ref string, publisher string, tag string) (string, error) {
+	// Create byte arrays
+	ZERO := CREATE_ZERO_BYTE_ARRAY()
+	ONE := CREATE_ONE_BYTE_ARRAY()
+
+	// Generate lookup key using Diffie Hellman
+	LOOKUP_KEY, err := al.diffieHellman.SharedSecret(publisher, tag, ZERO)
+	// Generate access key decryption key using Diffie Hellman
+	ACCESS_KEY_DECRYPTION_KEY, err := al.diffieHellman.SharedSecret(publisher, tag, ONE)
+
+	// Retrive MANIFEST from ACT
+	MANIFEST, err := al.act.GetFunc(act_root_hash)
+	// Lookup encrypted access key from the ACT manifest
+	ENCRYPTED_ACCESS_KEY := MANIFEST.SOME_LOOKUP_FUNCTION(LOOKUP_KEY)
+
+	// Decrypt access key
+	ACCESS_KEY := DECRYPT(ENCRYPTED_ACCESS_KEY, ACCESS_KEY_DECRYPTION_KEY)
+	// Decrypt reference
+	return DECRYPT(encryped_ref, ACCESS_KEY)
 }
 
 func NewAccessLogic(key encryption.Key, padding int, initCtr uint32, hashFunc func() hash.Hash) AccessLogic {
