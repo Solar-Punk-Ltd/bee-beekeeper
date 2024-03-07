18906c8839e62f5cb5ab0bca5e4e1aad59e52806
Peter Ott <ottpeter@tuta.io>
2024-03-07 14:45:09 +0100

    Adds manifest lookup

--- pkg/dynamicaccess/accesslogic.go
+++ pkg/dynamicaccess/accesslogic.go
@@ -1,11 +1,17 @@
 package dynamicaccess
 
 import (
+	"context"
 	"hash"
 
 	"github.com/ethersphere/bee/pkg/dynamicaccess/mock"
 	encryption "github.com/ethersphere/bee/pkg/encryption"
+	file "github.com/ethersphere/bee/pkg/file"
+	manifest "github.com/ethersphere/bee/pkg/manifest"
+	"golang.org/x/crypto/sha3"
 )
+
+var hashFunc = sha3.NewLegacyKeccak256
 
 type AccessLogic interface {
 	Get(act_root_hash string, encryped_ref string, publisher string, tag string) (string, error)
@@ -20,30 +26,56 @@ type DefaultAccessLogic struct {
 // Will give back Swarm reference with symmertic encryption key (128 byte)
 // @publisher: public key
 func (al *DefaultAccessLogic) Get(act_root_hash string, encryped_ref string, publisher string, tag string) (string, error) {
-	
+
 	// Create byte arrays
 	zeroByteArray := []byte{0}
 	oneByteArray := []byte{1}
 	// Generate lookup key using Diffie Hellman
 	lookup_key, err := al.diffieHellman.SharedSecret(publisher, tag, zeroByteArray)
-	if err!=nil {
+	if err != nil {
 		return "", err
 	}
 	// Generate access key decryption key using Diffie Hellman
 	access_key_decryption_key, err := al.diffieHellman.SharedSecret(publisher, tag, oneByteArray)
-	if err!=nil {
+	if err != nil {
 		return "", err
 	}
 	// Retrive MANIFEST from ACT
-	manifest, err := al.act.Get(act_root_hash)
+	manifest_raw, err := al.act.Get(act_root_hash)
+	if err != nil {
+		return "", err
+	}
 	al.act.Get(act_root_hash)
+
 	// Lookup encrypted access key from the ACT manifest
-	ENCRYPTED_ACCESS_KEY := SOME_LOOKUP_FUNCTION(manifest, lookup_key)
+	var loadSaver file.LoadSaver
+	var ctx context.Context
+	loadSaver.Load(ctx, []byte(manifest_raw)) // Load the manifest file into loadSaver
+	//y, err := x.Load(ctx, []byte(manifest_obj))
+	manifestObj, err := manifest.NewDefaultManifest(loadSaver, false)
+	if err != nil {
+		return "", err
+	}
+	encrypted_access_key, err := manifestObj.Lookup(ctx, lookup_key)
+	if err != nil {
+		return "", err
+	}
 
 	// Decrypt access key
-	ACCESS_KEY := DECRYPT(ENCRYPTED_ACCESS_KEY, access_key_decryption_key)
+	access_key_cipher := encryption.New(encryption.Key(access_key_decryption_key), 4096, uint32(0), hashFunc)
+	access_key, err := access_key_cipher.Decrypt(encrypted_access_key.Reference().Bytes())
+	if err != nil {
+		return "", err
+	}
+
 	// Decrypt reference
-	return DECRYPT(encryped_ref, ACCESS_KEY)
+	ref_cipher := encryption.New(access_key, 4096, uint32(0), hashFunc)
+	ref, err := ref_cipher.Decrypt([]byte(encryped_ref))
+	if err != nil {
+		return "", err
+	}
+
+	return string(ref), nil
 }
 
 func NewAccessLogic(key encryption.Key, padding int, initCtr uint32, hashFunc func() hash.Hash) AccessLogic {
@@ -54,7 +86,7 @@ func NewAccessLogic(key encryption.Key, padding int, initCtr uint32, hashFunc fu
 			},
 		},
 		encryption: encryption.New(key, padding, initCtr, hashFunc),
-		act: defaultAct{},
+		act:        defaultAct{},
 
 		// {
 		// 	AddFunc: func(ref string, publisher string, tag string) error {
@@ -66,10 +98,6 @@ func NewAccessLogic(key encryption.Key, padding int, initCtr uint32, hashFunc fu
 		// },
 	}
 }
-
-
-
-
 
 // -------
 // act: &mock.ContainerMock{
