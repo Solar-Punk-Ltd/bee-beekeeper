52e21b154254cc2b1f90e98f3879ca62de8d4363
Ferenc Sárai <ferenc.sarai@solarpunk.buzz>
2024-03-11 14:47:34 +0100

    Add TestLoadStore function to act_test.go

--- pkg/dynamicaccess/act_test.go
+++ pkg/dynamicaccess/act_test.go
@@ -22,3 +22,30 @@ func TestAddGet(t *testing.T) {
 		t.Errorf("Get() value is not the expected %s != %s", key, encryptedAccesskey)
 	}
 }
+
+func TestLoadStore(t *testing.T) {
+	act1 := dynamicaccess.NewDefaultAct()
+	lookupKey := swarm.RandAddress(t).Bytes()
+	encryptedAccesskey := swarm.RandAddress(t).Bytes()
+	actReturned := act1.Add(lookupKey, encryptedAccesskey)
+	if actReturned == nil {
+		t.Error("Add() should return an act")
+	}
+
+	act1String, err := act1.Store()
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Load serialized data into empty act2
+	act2 := dynamicaccess.NewDefaultAct()
+	err = act2.Load(act1String)
+	if err != nil {
+		t.Error(err)
+	}
+
+	key := act2.Get(lookupKey)
+	if key != hex.EncodeToString(encryptedAccesskey) {
+		t.Errorf("Get() value is not the expected %s != %s", key, encryptedAccesskey)
+	}
+}
