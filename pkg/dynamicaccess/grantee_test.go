598622dab89b5e71e089904cc06ec9d98614b876
Roland Seres <roland.seres90@gmail.com>
2024-03-19 11:42:21 +0100

    adding error handlings to grantee related tests

--- pkg/dynamicaccess/grantee_test.go
+++ pkg/dynamicaccess/grantee_test.go
@@ -8,72 +8,54 @@ import (
 	"testing"
 )
 
-// func TestGranteeRevoke(t *testing.T) {
-// 	err := NewGrantee().Revoke("")
-// 	if err != nil {
-// 		t.Errorf("Error revoking grantee: %v", err)
-// 	}
-// }
-
-/*func TestGranteeRevokeList(t *testing.T) {
-	_, err := NewGrantee().RevokeList("", nil, nil)
-	if err != nil {
-		t.Errorf("Error revoking list of grantees: %v", err)
-	}
-}*/
-
-// func TestGranteePublish(t *testing.T) {
-// 	err := NewGrantee().Publish("")
-// 	if err != nil {
-// 		t.Errorf("Error publishing grantee: %v", err)
-// 	}
-// }
-
 func TestGranteeAddGrantees(t *testing.T) {
-	// Create a new Grantee
 	grantee := NewGrantee()
 
-	// Generate some dummy ecdsa.PublicKey values
-	key1, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	key2, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	key1, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
 
-	// Add the keys to the grantee
+	key2, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
 	addList := []ecdsa.PublicKey{key1.PublicKey, key2.PublicKey}
 	grantees, err := grantee.AddGrantees(addList)
 
-	// Check for errors
 	if err != nil {
-		t.Fatalf("Expected no error, got %v", err)
+		t.Errorf("Expected no error, got %v", err)
 	}
 
-	// Check if the keys were added correctly
 	if !reflect.DeepEqual(grantees, addList) {
 		t.Errorf("Expected grantees %v, got %v", addList, grantees)
 	}
 }
 
 func TestRemoveGrantees(t *testing.T) {
-	// Create a new Grantee
 	grantee := NewGrantee()
 
-	// Generate some dummy ecdsa.PublicKey values
-	key1, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	key2, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	key1, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
 
-	// Add the keys to the grantee
+	key2, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
 	addList := []ecdsa.PublicKey{key1.PublicKey, key2.PublicKey}
 	grantee.AddGrantees(addList)
 
-	// Remove one of the keys
 	removeList := []ecdsa.PublicKey{key1.PublicKey}
 	grantees, err := grantee.RemoveGrantees(removeList)
 
-	// Check for errors
 	if err != nil {
-		t.Fatalf("Expected no error, got %v", err)
+		t.Errorf("Expected no error, got %v", err)
 	}
 
-	// Check if the key was removed correctly
 	expectedGrantees := []ecdsa.PublicKey{key2.PublicKey}
 	if !reflect.DeepEqual(grantees, expectedGrantees) {
 		t.Errorf("Expected grantees %v, got %v", expectedGrantees, grantees)
@@ -81,21 +63,23 @@ func TestRemoveGrantees(t *testing.T) {
 }
 
 func TestGetGrantees(t *testing.T) {
-	// Create a new Grantee
 	grantee := NewGrantee()
 
-	// Generate some dummy ecdsa.PublicKey values
-	key1, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	key2, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	key1, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
 
-	// Add the keys to the grantee
+	key2, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
 	addList := []ecdsa.PublicKey{key1.PublicKey, key2.PublicKey}
 	grantee.AddGrantees(addList)
 
-	// Get the grantees
 	grantees := grantee.GetGrantees()
 
-	// Check if the grantees were returned correctly
 	if !reflect.DeepEqual(grantees, addList) {
 		t.Errorf("Expected grantees %v, got %v", addList, grantees)
 	}
