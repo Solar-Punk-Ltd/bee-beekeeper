ee8da04fe7468a4fa65bd390fa17f72f2e93d301
Ferenc Sárai <ferenc.sarai@solarpunk.buzz>
2024-03-19 11:20:43 +0100

    (refactor): ACT interface params + add doc comments

--- pkg/dynamicaccess/act.go
+++ pkg/dynamicaccess/act.go
@@ -1,4 +1,10 @@
+// Copyright 2024 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package dynamicaccess
+
+// Package dynamicaccess provides a ACT mechanism for Swarm.
 
 import (
 	"encoding/hex"
@@ -7,10 +13,18 @@ import (
 	"github.com/ethersphere/bee/pkg/swarm"
 )
 
+// Act represents an interface for performing actions on a data store.
 type Act interface {
-	Add(lookupKey []byte, encryptedAccessKey []byte) Act
+	// Add adds a key-value pair to the data store.
+	Add(key []byte, value []byte) Act
+
+	// Lookup retrieves the value associated with the given lookup key from the data store.
 	Lookup(lookupKey []byte) []byte
+
+	// Load retrieves the manifest entry associated with the given lookup key from the data store.
 	Load(lookupKey []byte) manifest.Entry
+
+	// Store stores the given manifest entry in the data store.
 	Store(me manifest.Entry)
 }
 
@@ -20,13 +34,13 @@ type defaultAct struct {
 	container map[string]string
 }
 
-func (act *defaultAct) Add(lookupKey []byte, encryptedAccessKey []byte) Act {
-	act.container[hex.EncodeToString(lookupKey)] = hex.EncodeToString(encryptedAccessKey)
+func (act *defaultAct) Add(key []byte, value []byte) Act {
+	act.container[hex.EncodeToString(key)] = hex.EncodeToString(value)
 	return act
 }
 
-func (act *defaultAct) Lookup(lookupKey []byte) []byte {
-	if key, ok := act.container[hex.EncodeToString(lookupKey)]; ok {
+func (act *defaultAct) Lookup(key []byte) []byte {
+	if key, ok := act.container[hex.EncodeToString(key)]; ok {
 		bytes, err := hex.DecodeString(key)
 		if err == nil {
 			return bytes
@@ -35,12 +49,13 @@ func (act *defaultAct) Lookup(lookupKey []byte) []byte {
 	return make([]byte, 0)
 }
 
-// to manifestEntry
-func (act *defaultAct) Load(lookupKey []byte) manifest.Entry {
-	return manifest.NewEntry(swarm.NewAddress(lookupKey), act.container)
+// Load loads a manifest entry from the container based on the given key.
+// It returns the loaded manifest entry.
+func (act *defaultAct) Load(key []byte) manifest.Entry {
+	return manifest.NewEntry(swarm.NewAddress(key), act.container)
 }
 
-// from manifestEntry
+// Store stores a manifest entry in the container.
 func (act *defaultAct) Store(me manifest.Entry) {
 	if act.container == nil {
 		act.container = make(map[string]string)
@@ -48,6 +63,8 @@ func (act *defaultAct) Store(me manifest.Entry) {
 	act.container = me.Metadata()
 }
 
+// NewDefaultAct creates a new instance of the defaultAct struct.
+// It returns the Act interface.
 func NewDefaultAct() Act {
 	return &defaultAct{
 		container: make(map[string]string),
--- pkg/dynamicaccess/mock/act.go
+++ pkg/dynamicaccess/mock/act.go
@@ -1,3 +1,7 @@
+// Copyright 2024 The Swarm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package mock
 
 import (
@@ -6,33 +10,33 @@ import (
 )
 
 type ActMock struct {
-	AddFunc    func(lookupKey []byte, encryptedAccessKey []byte) dynamicaccess.Act
-	LookupFunc func(lookupKey []byte) []byte
-	LoadFunc   func(lookupKey []byte) manifest.Entry
+	AddFunc    func(key []byte, value []byte) dynamicaccess.Act
+	LookupFunc func(key []byte) []byte
+	LoadFunc   func(key []byte) manifest.Entry
 	StoreFunc  func(me manifest.Entry)
 }
 
 var _ dynamicaccess.Act = (*ActMock)(nil)
 
-func (act *ActMock) Add(lookupKey []byte, encryptedAccessKey []byte) dynamicaccess.Act {
+func (act *ActMock) Add(key []byte, value []byte) dynamicaccess.Act {
 	if act.AddFunc == nil {
 		return act
 	}
-	return act.AddFunc(lookupKey, encryptedAccessKey)
+	return act.AddFunc(key, value)
 }
 
-func (act *ActMock) Lookup(lookupKey []byte) []byte {
+func (act *ActMock) Lookup(key []byte) []byte {
 	if act.LookupFunc == nil {
 		return make([]byte, 0)
 	}
-	return act.LookupFunc(lookupKey)
+	return act.LookupFunc(key)
 }
 
-func (act *ActMock) Load(lookupKey []byte) manifest.Entry {
+func (act *ActMock) Load(key []byte) manifest.Entry {
 	if act.LoadFunc == nil {
 		return nil
 	}
-	return act.LoadFunc(lookupKey)
+	return act.LoadFunc(key)
 }
 
 func (act *ActMock) Store(me manifest.Entry) {
@@ -42,7 +46,7 @@ func (act *ActMock) Store(me manifest.Entry) {
 	act.StoreFunc(me)
 }
 
-func NewActMock(addFunc func(lookupKey []byte, encryptedAccessKey []byte) dynamicaccess.Act, getFunc func(lookupKey []byte) []byte) dynamicaccess.Act {
+func NewActMock(addFunc func(key []byte, value []byte) dynamicaccess.Act, getFunc func(key []byte) []byte) dynamicaccess.Act {
 	return &ActMock{
 		AddFunc:    addFunc,
 		LookupFunc: getFunc,
