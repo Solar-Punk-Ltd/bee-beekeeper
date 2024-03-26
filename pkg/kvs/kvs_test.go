879d025c83bbde01a677004851aead5cac1967ec
Ferenc Sárai <ferenc.sarai@solarpunk.buzz>
2024-03-26 13:57:06 +0100

    (test:) Add test for put/get after kvs.Save

--- pkg/kvs/kvs_test.go
+++ pkg/kvs/kvs_test.go
@@ -33,14 +33,17 @@ func createLs() file.LoadSaver {
 	return loadsave.New(mockStorer.ChunkStore(), mockStorer.Cache(), requestPipelineFactory(context.Background(), mockStorer.Cache(), false, redundancy.NONE))
 }
 
+func keyValuePair(t *testing.T) ([]byte, []byte) {
+	return swarm.RandAddress(t).Bytes(), swarm.RandAddress(t).Bytes()
+}
+
 func TestKvsAddLookup(t *testing.T) {
 	ls := createLs()
 
 	putter := mockStorer.DirectUpload()
 	s := kvs.New(ls, putter, swarm.ZeroAddress)
 
-	lookupKey := swarm.RandAddress(t).Bytes()
-	encryptedAccesskey := swarm.RandAddress(t).Bytes()
+	lookupKey, encryptedAccesskey := keyValuePair(t)
 
 	err := s.Put(lookupKey, encryptedAccesskey)
 	if err != nil {
@@ -62,8 +65,7 @@ func TestKvsAddLookupWithSave(t *testing.T) {
 	ls := createLs()
 	putter := mockStorer.DirectUpload()
 	s1 := kvs.New(ls, putter, swarm.ZeroAddress)
-	lookupKey := swarm.RandAddress(t).Bytes()
-	encryptedAccesskey := swarm.RandAddress(t).Bytes()
+	lookupKey, encryptedAccesskey := keyValuePair(t)
 
 	err := s1.Put(lookupKey, encryptedAccesskey)
 	if err != nil {
@@ -81,6 +83,50 @@ func TestKvsAddLookupWithSave(t *testing.T) {
 
 	if !bytes.Equal(key, encryptedAccesskey) {
 		t.Errorf("Get() value is not the expected %s != %s", hex.EncodeToString(key), hex.EncodeToString(encryptedAccesskey))
+	}
+
+}
+
+func TestKvsAddSaveAdd(t *testing.T) {
+	ls := createLs()
+	putter := mockStorer.DirectUpload()
+	kvs1 := kvs.New(ls, putter, swarm.ZeroAddress)
+	kvs1key1, kvs1val1 := keyValuePair(t)
+
+	err := kvs1.Put(kvs1key1, kvs1val1)
+	if err != nil {
+		t.Fatalf("Add() should not return an error: %v", err)
+	}
+	ref, err := kvs1.Save()
+	if err != nil {
+		t.Fatalf("Save() should not return an error: %v", err)
+	}
+
+	// New KVS
+	kvs2 := kvs.New(ls, putter, ref)
+
+	kvs2key1, kvs2val1 := keyValuePair(t)
+
+	// put after save
+	kvs2.Put(kvs2key1, kvs2val1)
+
+	// get after save
+	kvs2get1, err := kvs2.Get(kvs2key1)
+	if err != nil {
+		t.Fatalf("Lookup() should not return an error: %v", err)
+	}
+	if !bytes.Equal(kvs2get1, kvs2val1) {
+		t.Errorf("Get() value is not the expected %s != %s", hex.EncodeToString(kvs2key1), hex.EncodeToString(kvs2val1))
+	}
+
+	// get before Save
+	kvs2get2, err := kvs2.Get(kvs1key1)
+
+	if err != nil {
+		t.Fatalf("Lookup() should not return an error: %v", err)
+	}
+	if !bytes.Equal(kvs2get2, kvs1val1) {
+		t.Errorf("Get() value is not the expected %s != %s", hex.EncodeToString(kvs2get2), hex.EncodeToString(kvs1val1))
 	}
 
 }
